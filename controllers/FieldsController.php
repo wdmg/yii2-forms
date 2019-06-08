1e72d7c651cb75a76f2222b662c2cf891ee474e1
alex-wdmg <wdmg.com.ua@gmail.com>
2019-06-08 14:35:00 +0300

    Ignore RBAC roles if AuthManager not configured

--- controllers/FieldsController.php
+++ controllers/FieldsController.php
@@ -20,7 +20,7 @@ class FieldsController extends Controller
      */
     public function behaviors()
     {
-        return [
+        $behaviors = [
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
@@ -37,6 +37,21 @@ class FieldsController extends Controller
                 ],
             ],
         ];
+
+        // If auth manager not configured use default access control
+        if(!Yii::$app->authManager) {
+            $behaviors['access'] = [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'roles' => ['@'],
+                        'allow' => true
+                    ],
+                ]
+            ];
+        }
+
+        return $behaviors;
     }
 
     /**
